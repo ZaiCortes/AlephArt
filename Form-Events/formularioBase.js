cf0ae76183730cd8ca930216d351530f42e54d7b
ValLpz <val.lpz.glz@gmail.com>
2024-08-21 22:38:06 -0600

    cambios en css y html, creación de js con avances de funcionalidad

--- Form-Events/formularioBase.js
+++ Form-Events/formularioBase.js
@@ -1,77 +1,77 @@
 document.getElementById('formularioEvento').addEventListener('submit', function(event) {
-//Evitar que el formulario se envíe automáticamente
-event.preventDefault();
-//obtener valores de los campos
-const nombre = document.getElementById('nombre').value;
-const inputDate = document.getElementById('inputDate').value;
-const inputCity = document.getElementById('inputCity').value;
-const inputState = document.getElementById('inputState').value;
-const inputCategory = document.getElementById('inputCategory').value;
-const inputMode = document.getElementById('inputMode').value;
-const descripción = document.getElementById('descripción').value;
+    //Evitar que el formulario se envíe automáticamente
+    event.preventDefault();
+    //obtener valores de los campos
+    const nombre = document.getElementById('nombre').value;
+    const inputDate = document.getElementById('inputDate').value;
+    const inputCity = document.getElementById('inputCity').value;
+    const inputState = document.getElementById('inputState').value;
+    const inputCategory = document.getElementById('inputCategory').value;
+    const inputMode = document.getElementById('inputMode').value;
+    const descripción = document.getElementById('descripción').value;
  
-//Obtener referencias a los elementos error
-const nombreError = document.getElementById('nombre');
-const inputDateError = document.getElementById('inputDate');
-const inputCityError = document.getElementById('inputCity');
-const inputStateError = document.getElementById('inputState');
-const inputCategoryError= document.getElementById('inputCategory');
-const inputModeError = document.getElementById('inputMode');
-const descripciónError = document.getElementById('descripción');
+    //Obtener referencias a los elementos error
+    const nombreError = document.getElementById('nombre');
+    const inputDateError = document.getElementById('inputDate');
+    const inputCityError = document.getElementById('inputCity');
+    const inputStateError = document.getElementById('inputState');
+    const inputCategoryError= document.getElementById('inputCategory');
+    const inputModeError = document.getElementById('inputMode');
+    const descripciónError = document.getElementById('descripción');
 
-//limpiar los mensajes de error previos
-nombreError.textContent ='';
-inputDateError.textContent ='';
-inputCityError.textContent ='';
-inputStateError.textContent ='';
-inputCategoryError.textContent ='';
-inputModeError.textContent ='';
-descripciónError.textContent ='';
+    //limpiar los mensajes de error previos
+    nombreError.textContent ='';
+    inputDateError.textContent ='';
+    inputCityError.textContent ='';
+    inputStateError.textContent ='';
+    inputCategoryError.textContent ='';
+    inputModeError.textContent ='';
+    descripciónError.textContent ='';
 
-//Variable para verificar si hay errores
-let isValid = true;
+    //Variable para verificar si hay errores
+    let isValid = true;
 
-//validar el nombre
-if (nombre.tirim() === '') {
-    //alert('El nombre del evento es requerido');
-    isValid = false;
-}
-//validar la fecha
-if (inputDate.trim() === '') {
-    //alert ('La fecha es requerida. ');
-    isValid = false;
-}
-//validar la ciudad
-if (inputCity.length < 6) {
-    //alert ('La ciudad es requerida');
-    isValid = false;
-}
-//validar el estado
-if (inputState.trim() === '') {
-    // alert ('El estado es requerido');
-    isValid = false;
-}
-//validar la categoria
-if (inputCategory.trim() === '') {
-    //alert ('La categoría es requerida');
-    isValid = false;
-}
-//validar la modalidad
-if (inputMode.trim() === '') {
-    //alert ('La categoria es requerida');
-    isValid = false;
-}
-//validar la descripción
-if (descripción.length < 5) {
-    // alert ('La descripción es requerida');
-    isValid = false;
-}
-//Enviar formulario si es válido
-if(isValid) {
-    //ingresarshowAlert;
-}
+    //validar el nombre
+    if (nombre.tirim() === '') {
+        //alert('El nombre del evento es requerido');
+        isValid = false;
+    }
+    //validar la fecha
+    if (inputDate.trim() === '') {
+        //alert ('La fecha es requerida. ');
+        isValid = false;
+    }
+    //validar la ciudad
+    if (inputCity.length < 6) {
+        //alert ('La ciudad es requerida');
+        isValid = false;
+    }
+    //validar el estado
+    if (inputState.trim() === '') {
+        // alert ('El estado es requerido');
+        isValid = false;
+    }
+    //validar la categoria
+    if (inputCategory.trim() === '') {
+        //alert ('La categoría es requerida');
+        isValid = false;
+    }
+    //validar la modalidad
+    if (inputMode.trim() === '') {
+        //alert ('La categoria es requerida');
+        isValid = false;
+    }
+    //validar la descripción
+    if (descripción.length < 5) {
+        // alert ('La descripción es requerida');
+        isValid = false;
+    }
+    //Enviar formulario si es válido
+        if(isValid) {
+        //ingresarshowAlert;
+    }
 
-});
+}); //Función basada en el formulario de contacto
 
 
 
